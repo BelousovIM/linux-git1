266df6aa4de1810ed334d35ec4e489b9a59071cc
Белоусов_Илларион <bim159@mail.ru>
2020-11-05 14:50:47 +0000

    Add process.sh

--- process.sh
+++ process.sh
@@ -3,7 +3,7 @@ in2csv $1 | csvsql --query "select AVG(overall_ratingsource) from stdin where cl
 in2csv $1 | csvsql --query "select LOWER(country), COUNT(doc_id) from stdin GROUP BY 1"| awk -F, 'NR>1{print "HOTELNUMBER", $1, $2}'
 in2csv $1 | csvsql --query "select t1.country, holiday, hilton from (select lower(country) country, AVG(cleanliness) holiday from stdin where hotel_name like '%holiday inn%' and cleanliness>0 group by 1) t1 \
 left join (select lower(country) country, AVG(cleanliness) hilton from stdin where hotel_name like '%hilton%' and cleanliness>0 group by 1) t2 on t1.country==t2.country" | awk -F, 'NR>1{print "CLEANLINESS", $1, $2, $3}'
-awk -F, '{if($18>=0) print $12, $18}' $1 >> /tmp/BelousovIM_hotels.csv
+awk -F, '{if($18>=0 && $12>=0) print $12, $18}' $1 >> /tmp/BelousovIM_hotels.csv
 gnuplot -persist << -EOFMarker
         set terminal png size 300,400
         set datafile separator " "
