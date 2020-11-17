70ba1db8d06f2b61dec1d3c0f486cb143c024c3f
Белоусов_Илларион <bim159@mail.ru>
2020-11-17 19:15:00 +0000

    Add pulls.sh

--- pulls.sh
+++ pulls.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/bash
 H2="Authorization: token "`cat public_repo_token`
 
-#varAns=0
-#for x in 1 2
-#do
-#varx=$(curl -s -H "$H2" 'https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page='"$x"'&per_page=100'  | jq 'map(select(.user.login=="be-y-a")) | length')
-#varAns=$(($varAns+$varx))
-#done
-#echo "PULLS ${varAns}"
+varAns=0
+for x in 1 2
+do
+varx=$(curl -s -H "$H2" 'https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page='"$x"'&per_page=100'  | jq 'map(select(.user.login=="'"$1"'")) | length')
+varAns=$(($varAns+$varx))
+done
+echo "PULLS ${varAns}"
 
-
-#NEW=()
-#for x in 1 2
-#do
-#var=$(curl -s -H "$H2" 'https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page='"$x"'&per_page=100'  | jq 'map(select(.user.login=="be-y-a")) | .[].number')
-#NEW+=(`echo ${var[@]}`)
-#done
-#echo "EARLIEST ${NEW[0]}"
 
 NEW=()
 for x in 1 2
 do
-var=$(curl -s -H "$H2" 'https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page='"$x"'&per_page=100'  | jq 'map(select(.user.login=="be-y-a")) | .[].merged_at')
+var=$(curl -s -H "$H2" 'https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page='"$x"'&per_page=100'  | jq 'map(select(.user.login=="'"$1"'")) | .[].number')
+NEW+=(`echo ${var[@]}`)
+done
+echo "EARLIEST ${NEW[0]}"
+
+NEW=()
+for x in 1 2
+do
+var=$(curl -s -H "$H2" 'https://api.github.com/repos/datamove/linux-git2/pulls?state=all&page='"$x"'&per_page=100'  | jq 'map(select(.user.login=="'"$1"'")) | .[].merged_at')
 NEW+=(`echo ${var[@]}`)
 done
 if [ "${NEW[0]}" = "null" ]
